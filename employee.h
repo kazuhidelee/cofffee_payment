6299f9d8d321301d124106b67e0cd36da1b1958c
Tony Lee <tonyklee@umich.edu>
2024-02-15 23:26:12 -0500

    random

--- employee.h
+++ employee.h
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
+#include <random>
 
 using namespace std;
-
 // A struct that represent an employee
 struct employee
 {
@@ -22,8 +22,18 @@ struct employee_priority
 // Comparator that compares employees' priority in paying (needed for priority queue)
 struct employee_comparator
 {
+	mutable default_random_engine generator;
+
 	bool operator()(const employee_priority &employee_a, const employee_priority &employee_b) const
 	{
-		return employee_a.priority > employee_b.priority;
+		if (employee_a.priority == employee_b.priority)
+		{
+			uniform_int_distribution<int> distribution(0, 1);
+			return distribution(generator) == 0; // Randomly choose between the two when priorities are equal
+		}
+		else
+		{
+			return employee_a.priority > employee_b.priority;
+		}
 	}
 };
